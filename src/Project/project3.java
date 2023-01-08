5c091e0737f8ba1702e5af278150fd110cbfe0f6
Aisha1495 <aishaalmamari6@gmail.com>
2023-01-08 04:39:25 -0800

    enter number between limited numbers

---
+++ src/Project/project3.java
@@ -0,0 +1,27 @@
+package Project;
+
+import java.util.Scanner;
+
+public class project3 {
+	
+	public static void main(String[] arg) {
+
+Scanner sc = new Scanner (System.in);
+int result;
+do {
+	System.out.println("Enter Number btween 1- 100 ");
+	result =sc.nextInt();
+	
+	
+}
+while (result< 0 || result > 100);
+System.out.println("Correct "+result);
+
+//	
+//	while (result> 0 || result < 100);
+//System.out.println("Wrong "+result);
+	
+}
+}
+
+
