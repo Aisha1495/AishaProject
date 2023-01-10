d420858a74096669786c9bd3ad037b4e630f2070
Aisha1495 <aishaalmamari6@gmail.com>
2023-01-09 22:09:36 -0800

    merge two list in order

--- src/Project/project4.java
+++ src/Project/project4.java
@@ -6,27 +6,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class project4 {
-	public static void main(String[] args)
-	{
-		// q4 find  two number that sum to 6 from array list
-		
-		
-	ArrayList<Integer> numbers = new ArrayList<> ();
-	numbers.add(2);
-	numbers.add(3);
-	numbers.add(4);
-	numbers.add(5);
-	numbers.add(1);
-	numbers.add(7);
-	numbers.add(10);
-	
-	for ( int i = 0; i< numbers.size(); i++) {
-		for (int j = i +1; j < numbers.size(); j++) {
-			if (numbers.get(i)+ numbers.get(j)==6)
-			{
-				System.out.println("Found two numbers that sum to 6 : " + numbers.get(i)+" and " +numbers.get(j));}
+	public static void main(String[] args) {
+		// q4 find two number that sum to 6 from array list
+
+		ArrayList<Integer> numbers = new ArrayList<>();
+		numbers.add(2);
+		numbers.add(3);
+		numbers.add(4);
+		numbers.add(5);
+		numbers.add(1);
+		numbers.add(7);
+		numbers.add(10);
+
+		for (int i = 0; i < numbers.size(); i++) {
+			for (int j = i + 1; j < numbers.size(); j++) {
+				if (numbers.get(i) + numbers.get(j) == 6) {
+					System.out
+							.println("Found two numbers that sum to 6 : " + numbers.get(i) + " and " + numbers.get(j));
+				}
 			}
+		}
+
 	}
-	
-	}
-	}
+}
---
+++ src/Project/project6.java
@@ -0,0 +1,36 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class project6 {
+
+	public static void main(String[] args) {
+
+		// merge two list in order
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		arr.add(3);
+		arr.add(4);
+		arr.add(6);
+		arr.add(7);
+		arr.add(9);
+
+		ArrayList<Integer> arr1 = new ArrayList<Integer>();
+		arr1.add(1);
+		arr1.add(2);
+		arr1.add(5);
+		arr1.add(8);
+		arr1.add(10);
+
+		ArrayList<Integer> merge = new ArrayList<>();
+		for (int i = 0; i < arr.size(); i++) {
+			merge.add(arr.get(i));
+		}
+		for (int i = 0; i < arr1.size(); i++) {
+			merge.add(arr1.get(i));
+
+		}
+		merge.sort(null);
+		System.out.println(merge);
+	}
+
+}
