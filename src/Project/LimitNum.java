6b035e0b20eff9fcf94a75ac819ffd9c110a91c7
Aisha1495 <aishaalmamari6@gmail.com>
2023-01-09 23:34:12 -0800

    other answer for the output limited number

--- src/Project/LimitNum.java
+++ src/Project/LimitNum.java
@@ -7,20 +7,25 @@ public class LimitNum {
 	public static void main(String[] arg) {
 
 Scanner sc = new Scanner (System.in);
-int result;
-do {
-	System.out.println("Enter Number btween 1- 100 ");
-	result =sc.nextInt();
+int num1;
+int num2;
+while(true) {
+	System.out.println("Enter first Number ");
+	num1 =sc.nextInt();
 	
+	System.out.println("Enter Second Number ");
+	num2 =sc.nextInt();
 	
+	if (num1>= 0 && num1 <= 100 && num2 >= 0 && num2 <= 100) {
+		System.out.println("The enterd number is "+ num1 + " / "+ num2 + " Thank you. ");	
+		break;
+	}
+	else {
+		System.out.println(" The number is out of range 1-100 Please try again ");	
+	
+	}
 }
-while (result< 0 || result > 100);
-System.out.println("Correct "+result);
 
-	
-	while (result> 0 || result < 100);
-System.out.println("Wrong "+result);
-	
 }
 }
 
