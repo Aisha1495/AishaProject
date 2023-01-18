293d2a45cf5c4f706fecded48c012e8dcc9fff8b
Aisha1495 <aishaalmamari6@gmail.com>
2023-01-17 22:14:08 -0800

    list of integers as input to the stack

---
+++ src/Project/ListIntStack.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.util.Stack;
+
+public class ListIntStack {
+
+	public static void main(String[] args) {
+		//provide a list of integers as input to the stack
+
+		Stack<Integer> num = new Stack<>();
+		num.push(1);
+		num.push(2);
+		num.push(3);
+		num.push(4);
+		num.push(5);
+		num.push(6);
+		num.push(7);
+		System.out.println("List of integers : " + num);
+		
+	}
+
+}
