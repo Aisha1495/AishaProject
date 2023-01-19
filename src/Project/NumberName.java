ff5840ac45375dee46f294832d4cf83ac9d80dfd
Aisha1495 <aishaalmamari6@gmail.com>
2023-01-19 03:12:26 -0800

    store List of names with numbers

---
+++ src/Project/NumberName.java
@@ -0,0 +1,24 @@
+package Project;
+
+import java.util.HashMap;
+
+public class NumberName {
+
+	public static void main(String[] args) {
+	// Phone Book for Storing Multiple Phone Numbers for People
+		
+		HashMap<String, Integer> map = new HashMap<>();
+		map.put("Ali", 9997258);
+		map.put("Muna",9588598);
+		map.put("Sara", 9417998);
+		map.put("Amal", 9995278);
+		map.put("Ahmed", 9589998);
+		map.put("Alia", 9519498);
+		map.put("Abdullah", 9912398);
+		System.out.println(map);
+
+		
+		
+	}
+
+}
